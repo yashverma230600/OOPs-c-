9c6a027294e5278dd515f5ff8047db1e046c45f8
shadowk9 <verma.yash23.yv@gmail.com>
2022-03-26 21:55:54 +0530

    DAY 5

---
+++ printArrayFunc.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void printarr(int arr[5]){
+    cout<<"Printing elements"<<endl;
+
+    for(int i=0;i<5;i++){
+        cout<<arr[i]<<"\n";
+    }
+}
+
+int main(){
+    int arr[5] = {1,2,3,4,5};
+    printarr(arr);
+    return 0;
+}
