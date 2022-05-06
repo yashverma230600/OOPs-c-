a3c7860807b9a9b299990df1a10275cbde31a837
shadowk9 <verma.yash23.yv@gmail.com>
2022-05-07 00:43:52 +0530

    commit

---
+++ Doubepointer.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int i=5;
+    int* ptr=&i;
+    int** ptr2=&ptr;
+
+
+    cout<<endl<<"Sab sahi chal rha hai dost"<<endl<<endl;
+
+
+
+    cout<<"Printing 1st pointer ptr"<<ptr<<endl;
+    cout<<"address of 1st pointer ptr"<<&ptr<<endl;
+    cout<<"Printing 2nd pointer ptr2"<<ptr2<<endl;
+    cout<<"address of 2nd pointer ptr"<<&ptr2<<endl;
+
+return 0;
+}
